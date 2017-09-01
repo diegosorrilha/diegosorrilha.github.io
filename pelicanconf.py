50f1dbba66b78f1477cebb0d4b58e9b7cae2d481
Diego Sorrilha <diego@x4start.com>
2017-08-31 21:31:45 -0300

    reorganiza e seta mais configuracoes (urls, social)

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,18 +2,21 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+#### Base config ####
 AUTHOR = 'Diego Sorrilha'
 SITENAME = 'Diego Sorrilha'
 SITESUBTITLE = 'Tecnologia e outras viagens da cabeça'
 SITEURL = ''
+PATH = 'content'
+TIMEZONE = 'America/Sao_Paulo'
+DEFAULT_LANG = 'pt'
+DEFAULT_PAGINATION = 10
+
+
+#### Theme settings ####
 THEME_NAME = 'pelican-clean-blog'
 THEME = u'themes/'+ THEME_NAME
 
-PATH = 'content'
-
-TIMEZONE = 'America/Sao_Paulo'
-
-DEFAULT_LANG = 'pt'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_RSS = 'feeds.rss'
@@ -22,17 +25,37 @@ FEED_ALL_ATOM = 'feeds.atom'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom'
 FEED_USE_SUMMARY = True
 
-# Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
 
-# Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+#### Social ####
+GITHUB_URL = 'http://github.com/magnunleno/'
+LINKS =  (
+    ('Pelican', 'http://getpelican.com/'),
+    ('Python.org', 'http://python.org/'),
+    ('Jinja2', 'http://jinja.pocoo.org/'),
+)
+SOCIAL = (
+    ('envelope', 'mailto: diegopsorrilha@gmail.com'),
+    ('github', 'https://github.com/diegosorrilha'),
+    ('twitter', 'https://twitter.com/diegosorrilha'),
+    ('facebook', 'http://facebook.com/sorrilha.diego'),
+)
 
-DEFAULT_PAGINATION = 10
+
+#### Site URL writing ####
+ARTICLE_URL = "{slug}"
+ARTICLE_SAVE_AS = "{slug}/index.html"
+
+PAGE_URL = "{slug}"
+PAGE_SAVE_AS = "{slug}/index.html"
+
+CATEGORY_URL = "category/{slug}"
+CATEGORY_SAVE_AS = "category/{slug}/index.html"
+
+TAG_URL = "tag/{slug}"
+TAG_SAVE_AS = "tag/{slug}/index.html"
+
+AUTHOR_URL = 'author/{slug}'
+AUTHOR_SAVE_AS = 'author/{slug}/index.html'
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
