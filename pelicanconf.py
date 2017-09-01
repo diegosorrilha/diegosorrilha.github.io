7abb578526d607839995c0e07a4480f5c9448b9b
Diego Sorrilha <diego@x4start.com>
2017-08-31 21:02:24 -0300

    configura subtitulo e RSS

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,6 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = 'Diego Sorrilha'
 SITENAME = 'Diego Sorrilha'
+SITESUBTITLE = 'Tecnologia e outras viagens da cabeça'
 SITEURL = ''
 THEME_NAME = 'pelican-clean-blog'
 THEME = u'themes/'+ THEME_NAME
@@ -15,11 +16,11 @@ TIMEZONE = 'America/Sao_Paulo'
 DEFAULT_LANG = 'pt'
 
 # Feed generation is usually not desired when developing
-FEED_ALL_ATOM = None
-CATEGORY_FEED_ATOM = None
-TRANSLATION_FEED_ATOM = None
-AUTHOR_FEED_ATOM = None
-AUTHOR_FEED_RSS = None
+FEED_ALL_RSS = 'feeds.rss'
+CATEGORY_FEED_RSS = 'feeds/%s.rss'
+FEED_ALL_ATOM = 'feeds.atom'
+CATEGORY_FEED_ATOM = 'feeds/%s.atom'
+FEED_USE_SUMMARY = True
 
 # Blogroll
 LINKS = (('Pelican', 'http://getpelican.com/'),
